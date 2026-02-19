65b01a8fc4472af6044c3f1e54a7fb90667b6faa
Kenji Brameld <kbrameld@traclabs.com>
2026-02-19 17:08:52 +0000

    split bridge configs for the different demos

--- dragoman_ground_example/launch/lunar_exploration/lunar_exploration_ground.launch.py
+++ dragoman_ground_example/launch/lunar_exploration/lunar_exploration_ground.launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
         dragoman_ground_example_dir,
         "config", "lunar_exploration",
         "yamcs_bridge_params.yaml"
-    )    
+    )
 
     # RViz configuration file
     rviz_config = os.path.join(dragoman_ground_example_dir, "rviz", "lunar_exploration.rviz")
@@ -68,13 +68,18 @@ def generate_launch_description():
 
 
     # Yamcs-ROS Bridge - subscribes to Yamcs and publishes telemetry messages
-    yamcs_ros_bridge = IncludeLaunchDescription(
-      PathJoinSubstitution([FindPackageShare("dragoman_yamcs_ros_bridge"), "launch", "yamcs_ros_bridge.launch.py"]),
-      launch_arguments={
-        "use_sim_time": LaunchConfiguration("use_sim_time"),
-        "yamcs_url": LaunchConfiguration("yamcs_url"),
-        "bridge_config": LaunchConfiguration("bridge_config"),
-      }.items()
+    yamcs_ros_bridge = Node(
+        package="dragoman_yamcs_ros_bridge",
+        executable="yamcs_ros_bridge.py",
+        name="yamcs_curiosity_bridge",
+        output="screen",
+        arguments=[
+            "--config", LaunchConfiguration("bridge_config"),
+            "--yamcs-url", LaunchConfiguration("yamcs_url"),
+        ],
+        parameters=[
+            {"use_sim_time": LaunchConfiguration("use_sim_time")},
+        ],
     )
 
     # Joint State Publisher - converts MobileServicingSystemTelemetryPacket to JointState
